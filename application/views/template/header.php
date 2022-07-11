a25f64fe0ca9c0dc67fbfc8535b6da015534d5b8
n-azizah <62265999+n-azizah@users.noreply.github.com>
2022-07-11 23:07:57 +0800

    Membuat Surat Masuk

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,5 +10,11 @@
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+         <!-- Latest compiled and minified CSS -->
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+            <!-- (Optional) Latest compiled and minified JavaScript translation files -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script>
     </head>
     <body class="sb-nav-fixed">
