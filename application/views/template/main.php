82f934702d0bf522e7a2d1afec489212c36bd991
n-azizah <62265999+n-azizah@users.noreply.github.com>
2022-07-11 22:56:17 +0800

    Memperbaki template dan Menambahkan fitur user

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -1,6 +1,6 @@
 <?php
-$this->load->view('template/header');
-$this->load->view('template/navbar');
-$this->load->view('template/sidebar');
+$this->load->view('admin/template/header');
+$this->load->view('admin/template/navbar');
+$this->load->view('admin/template/sidebar');
 $this->load->view($content);
-$this->load->view('template/footer');
+$this->load->view('admin/template/footer'); 
